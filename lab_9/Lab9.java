08cca5e8bbd1e9bfe7aae7788599a7275dc1de7a
Petrova Polina <pa.petrova@air-soft.dev>
2022-12-04 19:35:57 +0400

    Новый файл Lab9 с решенным заданием

--- lab_9/Lab9.java
+++ lab_9/Lab9.java
@@ -1,3 +1,9 @@
+package lab_9;
+import java.util.regex.*;
+import java.util.*;
+import java.io.*;
+
+
 /*
 16. В программу подается текст из файла, в котором находится некий текст, либо набор текстов.
 Необходимо определить “настроение” текста. Это значит, что если в тексте будет употреблено много
@@ -5,5 +11,52 @@
 точно число “настроения”, для возможности сравнения различных текстов.
 */
 
+
+import java.util.*;
+
 public class Lab9 {
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<String>();
+        boolean flag = true;
+        int counter = 0;
+
+
+        // Добавление в список "негативных" слов
+        words.add("ткачиха");
+        words.add("повариха");
+        words.add("полотна");
+
+
+        // Чтение файла
+        try{
+            String path = "D://Projects//Java//regexp_lab//lab_9//data.txt";
+            File file = new File(path);
+
+            if(!file.exists()) file.createNewFile();
+
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line, word;
+
+            while((line = br.readLine()) != null) {
+                for (int i = 0; i < words.size(); i++) {
+                    word = words.get(i).toLowerCase();
+                    if(line.toLowerCase().contains(word)) counter += 1;
+                }
+            }
+
+            br.close();
+
+            // Определение настроения текста
+            if (counter > 10) flag = false;
+
+            if (flag) {
+                System.out.println("Положительное значение настроения текста:\nкол-во совпадений = " + counter);
+            } else {
+                System.out.println("Отрицательное значение настроения текста:\n кол-во совпадений = " + counter);
+            }
+
+        } catch(IOException e) {
+            System.out.print("Error: " + e);
+        }
+    }
 }
